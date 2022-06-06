f0d2375334082f6189cf43af637d8f3fceda4ac9
jasonlufa <cinfa@infinitiessoft.com>
2022-06-07 07:58:34 +0800

    setup all file and route, #4

--- 04-cocktail-gallery/src/App.js
+++ 04-cocktail-gallery/src/App.js
@@ -1,5 +1,25 @@
-function App() {
-  return <div className='App'></div>;
-}
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SingleCocktail from './pages/SingleCocktail';
+import Error from './pages/Error';
+import SharedLayout from './pages/SharedLayout';
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<SharedLayout />}>
+          <Route index element={<Home />}></Route>
+          <Route path='about' element={<About />}></Route>
+          <Route path='cocktail'>
+            <Route path=':productId' element={<SingleCocktail />}></Route>
+          </Route>
+          <Route path='*' element={<Error />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 export default App;
