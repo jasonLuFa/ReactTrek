cf4480c7f5f99297765891f4208c568272b4af1d
jasonlufa <cinfa@infinitiessoft.com>
2022-06-08 09:38:29 +0800

    CocktailList and Cocktail.j, #4

--- 04-cocktail-gallery/src/components/Cocktail.js
+++ 04-cocktail-gallery/src/components/Cocktail.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './css/cocktail.css';
 
-const Cocktail = () => {
-  return <div>Cocktail</div>;
+const Cocktail = ({ id, name, image, info, glass }) => {
+  return (
+    <article className='cocktail'>
+      <div className='image-container'>
+        <img src={image} alt={image} />
+      </div>
+      <div className='cocktail-footer'>
+        <h3>{name}</h3>
+        <h4>{glass}</h4>
+        <p>{info}</p>
+        <button className='btn btn-primary btn-details'>
+          <Link to={`/cocktail/${id}`}>details</Link>
+        </button>
+      </div>
+    </article>
+  );
 };
 
 export default Cocktail;
--- 04-cocktail-gallery/src/components/CocktailList.js
+++ 04-cocktail-gallery/src/components/CocktailList.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import Cocktail from './Cocktail';
+import { DotSpinner } from '@uiball/loaders';
+import { useGlobalContext } from '../context';
+import './css/cocktailList.css';
 
 const CocktailList = () => {
-  return <div>CocktailList</div>;
+  const { isLoading, cocktails } = useGlobalContext();
+  if (isLoading) {
+    return (
+      <div className='loading'>
+        <DotSpinner size={60} speed={0.9} color='#476a2e' />
+      </div>
+    );
+  }
+
+  if (cocktails.length < 1) {
+    return (
+      <h2 className='section-title warning'>
+        no cocktails matched your search criteria
+      </h2>
+    );
+  }
+  return (
+    <section>
+      <h2 className='section-title'>cocktails</h2>
+      <div className='cocktails-center'>
+        {cocktails.map((cocktail) => (
+          <Cocktail key={cocktail.id} {...cocktail} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default CocktailList;
